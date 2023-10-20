4d5ae45e3eb02495b48b2aa272d6cfc54451913a
cgHome <cghome@cflat-inc.org>
2023-10-20 09:04:28 +0200

    dim fixed

--- drivers/led/device.js
+++ drivers/led/device.js
@@ -54,8 +54,8 @@ module.exports = class LedDevice extends BaseDevice {
 
     const hue = (typeof valueObj.light_hue !== 'undefined') ? valueObj.light_hue : this.getCapabilityValue('light_hue');
     const saturation = (typeof valueObj.light_saturation !== 'undefined') ? valueObj.light_saturation : this.getCapabilityValue('light_saturation');
-    const color = tinycolor.fromRatio({ h: hue, s: saturation, v: 100 });
-    // const color = tinycolor.fromRatio({ h: hue, s: saturation, v: this.getCapabilityValue('dim') * 100 }); TODO: ?? Dim
+    const color = tinycolor.fromRatio({ h: hue, s: saturation, v: 100 }); // NOTE: Workaround until iolo has implemented dim
+    // const color = tinycolor.fromRatio({ h: hue, s: saturation, v: this.getCapabilityValue('dim') * 100 });
 
     return this.sendCommand('/led', color.toRgb())
       .then(() => this.triggerCapabilityListener('onoff', true))
@@ -75,9 +75,10 @@ module.exports = class LedDevice extends BaseDevice {
     const hue = Number((hsv.h / 359).toFixed(2));
     const saturation = Number(hsv.s.toFixed(2));
 
+    this.setCapabilityValue('onoff', data.on !== 0);
+    this.setCapabilityValue('dim', data.on === 0 ? 0 : 100); // NOTE: Workaround until iolo has implemented dim
     this.setCapabilityValue('light_hue', hue);
     this.setCapabilityValue('light_saturation', saturation);
-    // this.setCapabilityValue('dim', 0); // TODO:
   }
 
 };
